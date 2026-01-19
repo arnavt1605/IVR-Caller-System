13845e5fae5a723231ea9a2c47ae3ac2d3466ae1
Arnav <arnavtripathi1605@gmail.com>
2026-01-19 13:50:44 +0530

    feat: add helper function for app and security logging

---
+++ server/src/utils/logger.js
@@ -0,0 +1,24 @@
+// Helper functions for application and security logging
+
+import AppLog from "../models/AppLog.js";
+import SecurityLog from "../models/SecurityLog.js";
+
+export const logAppEvent = async ({ organizationId, level, message, route }) => {
+    try {
+        await AppLog.create({ organizationId, level, message, route });
+
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+
+export const logSecurityEvent = async ({ organizationId, userId, action, ip, userAgent }) => {
+    try {
+        await SecurityLog.create({ organizationId, userId, action, ip, userAgent });
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
