f8775225d687456f80cc5d71314e3a5c4f480e4b
Arnav <arnavtripathi1605@gmail.com>
2026-01-19 12:49:55 +0530

    feat: add business logic to select donors

---
+++ server/src/services/donor.service.js
@@ -0,0 +1,15 @@
+// Contains business logic for selecting donors for IVR calls
+
+import donor from "../models/donor.js";
+
+// Select the eligible donors
+export const selectDonorForCall = async ({ organizationId, bloodGroup, limit = 10 }) => {
+    const donors = await donors.find({ organizationId, bloodGroup, isActive: true }).sort({ lastCalledAt: 1 }).limit(limit);
+
+    return donors;
+}
+
+// Order of calling becomes:
+// 1. Never-called donors
+// 2. Least recently called donors
+// 3. Most recently called donors (last)
